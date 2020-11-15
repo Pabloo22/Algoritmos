3e749c959b401fd59fd9de09debe8ca329ef3dfb
Pablo <72697714+Pabloo22@users.noreply.github.com>
2020-11-15 10:29:52 +0100

    Update algoritmo_ordenacion_topologico.py

--- algoritmo_ordenacion_topologico.py
+++ algoritmo_ordenacion_topologico.py
@@ -8,14 +8,12 @@ def AOT(grafo: dict, l_ordenada = []): # Algoritmo de ordenacion topológico
 			if set(grafo[p]) & set(l_ordenada) == set(grafo[p]):
 				l_ordenada.append(p)
 
-	
 	return AOT(grafo, l_ordenada)
 
 # Ejemplo
 grafo = {"P1": [], "P2": [], 
-		"P3": ["P2"], "P4": ["P1", "P2"], "P5": ["P1"], 
-		"P6": ["P7", "P4", "P3"], "P7": ["P5", "P4"], 
-		"P8": ["P7"], "P9": ["P8", "P6"]}
-
+	"P3": ["P2"], "P4": ["P1", "P2"], "P5": ["P1"], 
+	"P6": ["P7", "P4", "P3"], "P7": ["P5", "P4"], 
+	"P8": ["P7"], "P9": ["P8", "P6"]}
 
 print(AOT(grafo))
