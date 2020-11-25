f327e0511dc482341548959403c2a40bc5f6cda2
Pablo <72697714+Pabloo22@users.noreply.github.com>
2020-11-25 18:36:59 +0100

    Update cronometro_de_funciones.py

--- cronometro_de_funciones.py
+++ cronometro_de_funciones.py
@@ -2,7 +2,7 @@ from time import time
 # import sys
 # sys.setrecursionlimit(10000)
 
-def tiempo(funcion):
+def cronometra(funcion):
 
 	def print_tiempo(*args, rep = 1_000_000):
 
@@ -21,7 +21,7 @@ Nota: si la función es recursiva necesitaremos una copia de ella en la propia
 función puesto que si no cuando se llame así misma estaría llamándose con el decorador.
 """
 # Ejemplo:
-@tiempo
+@cronometra
 def multiply(y, z):
 
 	def multiply_copy(y, z):
@@ -39,7 +39,7 @@ def multiply(y, z):
 	else:
 		return multiply_copy(2*y, z//2)
 
-@tiempo
+@cronometra
 def multiply2(y, z): return y*z
 
 # No se requiere de print()
