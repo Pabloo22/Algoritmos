58ee9ffc2c08cf2720bd8627075a9834cd70273a
Pablo <72697714+Pabloo22@users.noreply.github.com>
2021-02-11 23:24:06 +0100

    Add files via upload

---
+++ Grafos.py
@@ -0,0 +1,23 @@
+
+
+def check_sequence(sequence):
+	""" This algorithm is based on the Havel Hakimi theorem.
+	Returns True if the sequence is a graphic and returns False
+	if it isn't.
+	"""
+
+	while True:
+
+		sequence.sort(reverse=True)  # O(n log n)
+		first = sequence.pop(0)
+
+		for i in range(len(sequence[:first])):
+			sequence[i] -= 1
+
+		if -1 in sequence:
+			return False
+		elif max(sequence) == 0:
+			return True
+
+
+
